de792e8f0256cbe379939c24f71c94da61f467fb
Marcello Feroce <ferocemarcello@gmail.com>
2024-08-28 09:12:38 +0200

    Create fire_wallet_value_calc.py

---
+++ fire_wallet_value_calc.py
@@ -0,0 +1,9 @@
+import sys
+
+# Read user input from command line arguments
+inter_month = float(sys.argv[1])
+sell_month = float(sys.argv[2])
+tax = float(sys.argv[3])
+
+wv = (sell_month*(1-tax))/(inter_month-1)
+print("wallet value to earn exactly what you sell per month: "+str(wv)

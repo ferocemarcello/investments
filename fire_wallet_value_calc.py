602860cb3c13a5031587b0bcb9f54863efa0b885
Marcello Feroce <ferocemarcello@gmail.com>
2024-08-28 09:46:08 +0200

    Update fire_wallet_value_calc.py

--- fire_wallet_value_calc.py
+++ fire_wallet_value_calc.py
@@ -6,4 +6,4 @@ sell_month = float(sys.argv[2])
 tax = float(sys.argv[3])
 
 wv = (sell_month*(1-tax))/(inter_month-1)
-print("wallet value to earn exactly what you sell per month: "+str(wv)
+print("wallet value to earn exactly what you sell per month: "+str(wv))

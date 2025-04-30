91060f43341f8ef9c49c2bbe2262b13b2fcfe962
Marcello Feroce <ferocemarcello@gmail.com>
2025-04-30 15:41:58 +0200

    Update roi_year.py

--- roi_year.py
+++ roi_year.py
@@ -1,25 +1,27 @@
 import sys
 
 # Read user input from command line arguments
-value = float(sys.argv[1])
+initial_value = float(sys.argv[1])
 deposit_year = float(sys.argv[2])
-number_years = float(sys.argv[3])
+number_years = int(sys.argv[3])
 inter_year = float(sys.argv[4])
 tax = float(sys.argv[5])
 
 deposit_year_list = [deposit_year]*number_years
-
-for i in range(len(deposit_year)):
-    value+=deposit_year_list[i]
-    init_value+=deposit_year_list[i]
-    value = value * inter_year
-    print("after "+str(i+1)+" years, wallet value: " + str(value) +"\ndeposited value: "+ str(init_value))
-    print("ROI: " + str(value/init_value))
-    yearly_gain_taxed = (1-tax)*value
+wallet_value = initial_value
+deposited_value = initial_value
+count_years = 1
+for deposit in deposit_year_list:
+    wallet_value+=deposit
+    deposited_value+=deposit
+    wallet_value = wallet_value * inter_year
+    print("after "+str(count_years)+" years, wallet value: " + str(wallet_value) +"\ndeposited value: "+ str(deposited_value))
+    print("ROI: " + str(wallet_value/deposited_value))
+    yearly_gain_taxed = (1-tax)*wallet_value
     print("Monthly gain, taxed: " + str
     (yearly_gain_taxed))
     print("")
+    count_years +=1
 
-tax=0.33
-taxed_value = (1-tax)*value
-print("value when withdrawed after taxes: "+str(taxed_value))
+taxed_value = (1-tax)*wallet_value
+print("Wallet value when withdrawed after taxes: "+str(taxed_value))
